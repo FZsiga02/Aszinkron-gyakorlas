473252af1c8de5822310521e0090fa7c8859e3cc
FZsiga02 <fzsigmond02@gmail.com>
2022-11-08 10:06:09 +0100

    Táblázat

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
     let sulyos = document.getElementById('sulyos');
     let suly = document.getElementById('suly');
     let barna = document.getElementById('barna');
-    let tablazat = document.getElementById('tablazat');
+    
 
     osszes.addEventListener('click', async () => {
         let response = await fetch('users.json');
         let result = await response.json();
         megjelenites(result.users);
+    })
+
+    elerhetosegek.addEventListener('click', async () => {
+        let response = await fetch('users.json');
+        let result = await response.json();
+        elerhetoseg(result.users);
     })
 
     function megjelenites(felhasznalok){
@@ -20,6 +26,24 @@ document.addEventListener('DOMContentLoaded', () => {
             let li = document.createElement('li');
             li.innerHTML = f.firstName + " " + f.lastName + "<hr>";
             felhasznalokLista.appendChild(li);
+        }
+    }
+
+    function elerhetoseg(felhasznalok){
+        let tablazat = document.getElementById('tablazat');
+        tablazat.textContent = '';
+
+        for (let f of felhasznalok){
+            let tr = tablazat.insertRow();
+            let td = tr.insertCell();
+            let td2 = tr.insertCell();
+            let td3 = tr.insertCell();
+            td.appendChild(document.createTextNode(f.username));
+            td.style.border = '1px solid black';
+            td2.appendChild(document.createTextNode(f.email));
+            td2.style.border = '1px solid black';
+            td3.appendChild(document.createTextNode(f.phone));
+            td3.style.border = '1px solid black';
         }
     }
 })
